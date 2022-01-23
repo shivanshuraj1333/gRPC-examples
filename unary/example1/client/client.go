228068309986119a86793e96bffd9e81da3bf76f
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2022-01-23 18:37:24 +0530

    added client for unary

--- unary/example1/client/client.go
+++ unary/example1/client/client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"github.com/shivanshu1333/gRPC-examples/unary/example1/greetpb"
 	"google.golang.org/grpc"
 	"log"
 )
@@ -9,11 +11,28 @@ const (
 	address = "0.0.0.0:50051"
 )
 
-func main(){
+func makeUnaryCall(c greetpb.GreetServiceClient) {	req := &greetpb.GreetRequest{
+	Greeting: &greetpb.Greeting{
+		FirstName: "shivanshu",
+		LastName: "shrivastava",
+	},
+}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error occured, %v", err)
+	}
+	log.Printf("Response from server: %v", res.Result)
+}
+
+func main() {
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Printf("Error occured")
+		log.Fatalf("Error occured %v", err)
 	}
 	defer conn.Close()
 
+	c := greetpb.NewGreetServiceClient(conn)
+
+	makeUnaryCall(c)
 }
+
