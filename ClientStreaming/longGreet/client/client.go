9074e7eb0ebc63ae9c614807ee640901163d7912
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2022-01-24 14:02:05 +0530

    refactoring

--- ClientStreaming/longGreet/client/client.go
+++ ClientStreaming/longGreet/client/client.go
@@ -1,1 +1,77 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/shivanshu1333/gRPC-examples/ClientStreaming/longGreet/proto"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func doClientStreaming(c proto.GreetServiceClient) {
+	fmt.Println("Starting to do a Client Streaming RPC...")
+
+	requests := []*proto.LongGreetRequest{
+		&proto.LongGreetRequest{
+			Greeting: &proto.Greeting{
+				FirstName: "Stephane",
+			},
+		},
+		&proto.LongGreetRequest{
+			Greeting: &proto.Greeting{
+				FirstName: "John",
+			},
+		},
+		&proto.LongGreetRequest{
+			Greeting: &proto.Greeting{
+				FirstName: "Lucy",
+			},
+		},
+		&proto.LongGreetRequest{
+			Greeting: &proto.Greeting{
+				FirstName: "Mark",
+			},
+		},
+		&proto.LongGreetRequest{
+			Greeting: &proto.Greeting{
+				FirstName: "Piper",
+			},
+		},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling LongGreet: %v", err)
+	}
+
+	// we iterate over our slice and send each message individually
+	for _, req := range requests {
+		fmt.Printf("Sending req: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving response from LongGreet: %v", err)
+	}
+	fmt.Printf("LongGreet Response: %v\n", res)
+
+}
+
+const (
+	address = "0.0.0.0:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Error occured %v", err)
+	}
+	defer conn.Close()
+
+	c := proto.NewGreetServiceClient(conn)
+
+	doClientStreaming(c)
+}
--- ClientStreaming/longGreet/server/server.go
+++ ClientStreaming/longGreet/server/server.go
@@ -1,1 +1,52 @@
-package server
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"github.com/shivanshu1333/gRPC-examples/ClientStreaming/longGreet/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	proto.UnimplementedGreetServiceServer
+}
+
+func (*server) LongGreet(stream proto.GreetService_LongGreetServer) error {
+	fmt.Printf("LongGreet function was invoked with a streaming request\n")
+	result := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stream
+			return stream.SendAndClose(&proto.LongGreetResponse{
+				Result: result,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+
+		firstName := req.GetGreeting().GetFirstName()
+		result += "Hello " + firstName + "! "
+	}
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
