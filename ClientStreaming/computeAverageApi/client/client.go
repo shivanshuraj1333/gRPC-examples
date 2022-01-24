473f3c3eeb4eff7bbd1b86326bbf26ee6836b2aa
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2022-01-24 14:16:29 +0530

    added client streaming computeAverage

--- ClientStreaming/computeAverageApi/client/client.go
+++ ClientStreaming/computeAverageApi/client/client.go
@@ -1,1 +1,87 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/shivanshu1333/gRPC-examples/ClientStreaming/computeAverageApi/proto"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func doClientStreaming(c proto.ComputeServiceClient) {
+	fmt.Println("Starting to do a Client Streaming RPC...")
+
+	requests := []*proto.Request{
+		{
+			Value : &proto.Compute{
+				Val: 1,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 2,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 3,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 4,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 5,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 6,
+			},
+		},
+		{
+			Value : &proto.Compute{
+				Val: 7,
+			},
+		},
+	}
+
+	stream, err := c.Compute(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling LongGreet: %v", err)
+	}
+
+	// we iterate over our slice and send each message individually
+	for _, req := range requests {
+		fmt.Printf("Sending req: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving response from LongGreet: %v", err)
+	}
+	fmt.Printf("LongGreet Response: %v\n", res)
+
+}
+
+const (
+	address = "0.0.0.0:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Error occured %v", err)
+	}
+	defer conn.Close()
+
+	c := proto.NewComputeServiceClient(conn)
+
+	doClientStreaming(c)
+}
--- ClientStreaming/computeAverageApi/server/server.go
+++ ClientStreaming/computeAverageApi/server/server.go
@@ -1,1 +1,57 @@
-package server
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strconv"
+
+	"github.com/shivanshu1333/gRPC-examples/ClientStreaming/computeAverageApi/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	proto.UnimplementedComputeServiceServer
+}
+
+func (*server) Compute(stream proto.ComputeService_ComputeServer) error {
+	fmt.Printf("Compute function was invoked with a streaming request\n")
+	totalVal, totalReq := int64(0), int64(0)
+	result := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stream
+			avgVal := totalVal/totalReq
+			result = "Compute avg is " + strconv.Itoa(int(avgVal))
+			return stream.SendAndClose(&proto.Response{
+				Result: result,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+
+		val := req.GetValue().GetVal()
+		totalVal += val
+		totalReq++
+	}
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterComputeServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
