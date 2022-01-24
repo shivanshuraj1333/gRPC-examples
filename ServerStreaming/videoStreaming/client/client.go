a451f2b5fbffa2febf0005abd0333fcc73d4627f
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2022-01-24 12:44:21 +0530

    added server streaming example

--- ServerStreaming/videoStreaming/client/client.go
+++ ServerStreaming/videoStreaming/client/client.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/shivanshu1333/gRPC-examples/ServerStreaming/videoStreaming/proto"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+)
+
+func doServerStreaming(c proto.StreamServiceClient) {
+	fmt.Println("Starting to do a streaming RPC...")
+
+	req := &proto.StreamRequest{
+		Stream: &proto.VideoName{
+			VideoName: "kubeconEU2022",
+		},
+	}
+	resStream, err := c.Stream(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error occured %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// we've reached the end of stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Printf("Response: %v", msg.GetResult())
+	}
+}
+
+const (
+	address = "0.0.0.0:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Error occured %v", err)
+	}
+	defer conn.Close()
+
+	c := proto.NewStreamServiceClient(conn)
+
+	doServerStreaming(c)
+}
--- ServerStreaming/videoStreaming/server/server.go
+++ ServerStreaming/videoStreaming/server/server.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"github.com/shivanshu1333/gRPC-examples/ServerStreaming/videoStreaming/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+type server struct{
+	proto.UnimplementedStreamServiceServer
+}
+
+func (s *server) Stream(req *proto.StreamRequest, stream proto.StreamService_StreamServer) error {
+	fmt.Printf("Received a streaming request %v\n", req)
+	videoName := req.GetStream().GetVideoName()
+	for i := 0; i < 10; i++ {
+		result := "Started streaming service " + videoName + " res id: " + strconv.Itoa(i)
+		res := &proto.StreamResponse{
+			Result: result,
+		}
+		err := stream.Send(res)
+		if err != nil {
+			return err
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return nil
+}
+
+const (
+	address = "0.0.0.0:50051"
+)
+
+func main() {
+	fmt.Println("Basic ServerStreaming Service initiating....")
+
+	lis, err := net.Listen("tcp", address)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterStreamServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
